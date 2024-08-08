91372bdaf56f296621cfda9d2acf2e777cdaaa43
colmedev <m.colmenarez.dev@gmail.com>
2024-08-07 23:14:22 -0400

    feat: :fire: remove personal api key

--- gemini.mjs
+++ gemini.mjs
@@ -1,4 +1,4 @@
-const apiKey = 'AIzaSyDlx3BQvFlf-Nv3no_a7qIv_DvUVjeimXg';
+const apiKey = '';
 const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
 
 const data = {
@@ -21,5 +21,5 @@ fetch(url, {
   body: JSON.stringify(data)
 })
 .then(response => response.json())
-.then(data => console.log(data.content))
+.then(data => console.log(data))
 .catch(error => console.error('Error:', error));
